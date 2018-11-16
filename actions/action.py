c7372dc645096566e126ff80dd445a06292a9f6c
martezr <martez.reed@greenreedtech.com>
2018-11-16 16:06:30 -0600

    Initial code commit

---
+++ actions/action.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from st2common.runners.base_action import Action
+import tower_cli
+from tower_cli.conf import settings
+
+class AnsibleTowerAction(Action):
+
+    def __init__(self, config):
+        super(AnsibleTowerAction, self).__init__(config)
+
+    def _get_client(self):
+        hostname = self.config['hostname']
+        username = self.config['username']
+        password = self.config['password']
+
+        settings.runtime_values(hostname, username, password)
+        return client
